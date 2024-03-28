3c8e985d5389092e7db6a81f9c34f11c1782c655
mjzfromdastreets <mahjz92@gmail.com>
2024-03-28 17:29:06 -0400

    Added scripts for great-lakes
    - Added `submit_train.sh` used to submit train job to slurm
    
    - Added setup scripts to create environment needed to run spatial intention maps

---
+++ great_lakes_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git clone https://github.com/mushroonhead/spatial-intention-maps.git
+
+module purge
+module load python/3.10.4
+module load cuda/11.8.0
+
+mkdir envs/
+cd envs/spatial_int_maps_3_10_4
+python -m venv spatial_int_maps_3_10_4
+source spatial_int_maps_3_10_4/bin/activate
+
+pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu118
+pip install pybullet tensorboard future scipy scikit-image pyyaml munch pandas tqdm prompt-toolkit Cython opencv-python pyqt5 fpdf matplotlib
+# pip install anki-vector zeroconf opencv-contrib-python pyglet
+cd ../spatial-intention-maps/shortest_paths
+python setup.py build_ext --inplace
+
+deactivate
+module purge
---
+++ submit_train.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=train_model              # create a short name for your job
+#SBATCH --output=logs/%x-%j.log           	# Standard output in run directory, formatted: jobName-jobID.log
+#SBATCH --error=logs/%x-%j-E.log           	# Standard error log
+#SBATCH --account=eecs602w24_class         	# account (check with $ my_accounts)
+#SBATCH --partition=spgpu                   # (Submit to partition: standard, gpu. viz, largemem, oncampus, debug)
+#SBATCH --gres=gpu:1                      	# GPUs per node
+#SBATCH --time=0-6:00:00                    # total run time limit (dd-hh:mm:ss)
+#SBATCH --nodes=1                         	# node count
+#SBATCH --tasks-per-node=1                	# total number of tasks across all nodes
+#SBATCH --cpus-per-task=1                 	# cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=60gb                         	# total memory per node
+#SBATCH --mail-type=END,FAIL              	# send mail if job fails
+#SBATCH --export=ALL                      	# Copy environment
+
+# Load modules
+module purge
+module load python/3.10.4
+module load cuda/11.8.0
+source /home/joshmah/spatial_int_map_diffuser/envs/spatial_int_maps_3_10_4/bin/activate
+
+# run trainer
+python train.py --config-path /home/joshmah/spatial_int_map_diffuser/config/ours/pushing_4-small_divider-ours.yml
+
+# close modules
+deactivate
+module purge
