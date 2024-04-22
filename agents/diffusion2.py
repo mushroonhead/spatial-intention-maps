Fixed qmap calculation, to run model and test again
- Fixed qmap to only update value after selecting the right action from the qmap

- Added another light unet like layer for image synthesis

agents/diffusion2.py
@@ -169,7 +169,7 @@ class TDErrorQMapDiffTrainer(DiffTrainerBase):
         self.current_tau = torch.nn.Parameter(torch.tensor(init_tau), requires_grad=False) # track current tau
         self.tau_decay = tau_decay
         self.target_transfer_period = target_transfer_period
-        self.qmap_loss = qmap_loss
+        self.td_loss = qmap_loss
         self.boot_strapped_policy = boot_strapped_policy
 
     def train(self, training_iter: int,
@@ -192,14 +192,14 @@ class TDErrorQMapDiffTrainer(DiffTrainerBase):
 
         # standard dqn td error training (here we assume r to be determined only on s,a)
         with torch.no_grad():
-            next_step_vals = self.target_policy(non_final_next_states).detach()
-            next_state_shape = next_step_vals.shape[1:]
-            target_q_map = rewards[:,None,None,None] + (self.discount_factor * \
-                torch.eye(B, device=device)[non_final_state_mask].T @ next_step_vals.view(-1,prod(next_state_shape))
-                ).view(B,*next_state_shape)
-        pred_q_map = self.policy(state_map)
+            next_vals = self.target_policy(non_final_next_states).detach().view(B,-1).max(-1)[0] # (B,)
+            next_vals = rewards + (self.discount_factor * \
+                torch.eye(B, device=device)[non_final_state_mask].T @ next_vals)
 
-        td_loss: torch.Tensor = self.qmap_loss(pred_q_map, target_q_map)
+        pred_q_map = self.policy(state_map)
+        current_vals = pred_q_map.view(B, -1).gather(1, actions.unsqueeze(1)).squeeze(1)
+
+        td_loss: torch.Tensor = self.td_loss(current_vals, next_vals)
 
         # diffusion loss
         if self.boot_strapped_policy is None:
@@ -419,6 +419,7 @@ class ActionDiffuserTrainer(DiffTrainerBase):
                  critic_optim_type=torch.optim.Adam,
                  critic_optim_params={'lr':1e-4, 'weight_decay': 1e-5},
                  critic_loss=torch.nn.MSELoss(),
+                 clipped_grad_norm=1.0,
                  action_channel = 1,
                  height = 96, width= 96
                  ) -> None:
@@ -433,6 +434,7 @@ class ActionDiffuserTrainer(DiffTrainerBase):
         self.update_ema_every = update_ema_every
         self.critic_xfer_period = critic_xfer_period
         self.critic_tau = critic_tau
+        self.clipped_grad_norm = clipped_grad_norm
         self.height = height
         self.width = width
         self.action_channel = action_channel
@@ -494,6 +496,8 @@ class ActionDiffuserTrainer(DiffTrainerBase):
 
         # backpropagation
         actor_loss.backward()
+        actor_grad_norms = torch.nn.utils.clip_grad_norm_(
+            self.actor.parameters(), max_norm=self.clipped_grad_norm, norm_type=2)
         self.optimizer.step()
         self.optimizer.zero_grad()
         self.lr_scheduler.step()

networks.py
@@ -228,19 +228,48 @@ class CondMLP(torch.nn.Module):
 
         return x
 
+################### Image net 2
+
+class LightCondUnet2D(torch.nn.Module):
+    def __init__(self,
+                    encoder_channel: int,
+                    inp_channel: int,
+                    t_dim: int,
+                    *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.a_enc = resnet.resnet_N(layers=[2,2,2], features_only=True,
+                                        num_input_channels=inp_channel)
+        self.time_enc = nn.Sequential(
+            SinusoidalPosEmb(t_dim),
+            nn.Linear(t_dim, t_dim * 2),
+            nn.ReLU(),
+            nn.Linear(t_dim * 2, t_dim))
+        stacked_channels = 256 + encoder_channel + t_dim # 256 det by layers=[2,2,2]
+        self.dec = nn.Sequential(
+            torch.nn.Conv2d(stacked_channels, 256, kernel_size=1, stride=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.Mish(),
+            Interpolate({'scale_factor':2, 'mode':'bilinear', 'align_corners':True}),
+            torch.nn.Conv2d(256, 64, kernel_size=1, stride=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.Mish(),
+            Interpolate({'scale_factor':2, 'mode':'bilinear', 'align_corners':True}),
+            torch.nn.Conv2d(64, inp_channel, kernel_size=1, stride=1))
+
+    def forward(self, sample: torch.Tensor, timestep: torch.IntTensor,
+                encoder_hidden_states: torch.Tensor) -> torch.Tensor:
+        a_enc = self.a_enc(sample) # (B,a_enc,24,24)
+        if timestep.ndim < 1:
+            timestep = timestep.unsqueeze(-1) #(1,)
+        t_enc = self.time_enc(timestep.to(sample.device))[...,None,None].expand(
+            a_enc.shape[0],-1,*a_enc.shape[-2:]) # (B,t_dim,24,24)
+        x = torch.cat((a_enc, encoder_hidden_states, t_enc), dim=-3)
+        x = self.dec(x)
+
+        return x
 
 if __name__ == '__main__':
-    state = torch.randn(32, 5, 96,96)
-    action = torch.randn(32,2)
-    t = torch.randint(0,100, (32,))
 
-    resf = resnet.resnet_N(layers=[2,], features_only=True, num_input_channels=5)
-    enc = resf(state)
-    cond_mlp = CondMLP(64*24*24, 2, 16)
-    pred_noise = cond_mlp(sample=action, timestep=t,
-                          encoder_hidden_states=enc)
-
-    print(pred_noise.shape)
 
 
     # # light bottleneck
@@ -249,6 +278,8 @@ if __name__ == '__main__':
     # model = LightWeightBottleneck(C)(x)
 
     # x.shape
+
+    ############################
 
     # # test LightUnet
     # C = 5
@@ -269,6 +300,8 @@ if __name__ == '__main__':
     #                                     encoder_hidden_states=cond)
     # print(y.shape)
 
+
+    ##########################
     # C = 5
 
     # x = torch.randn(32, C, 96, 96)
@@ -288,5 +321,35 @@ if __name__ == '__main__':
     # lfcn = LightEncoderFCN(C)
     # y = lfcn(x)
     # print(y.shape)
+
+    ###################
+
+    # state = torch.randn(32, 5, 96,96)
+    # action = torch.randn(32,2)
+    # t = torch.randint(0,100, (32,))
+
+    # resf = resnet.resnet_N(layers=[2,], features_only=True, num_input_channels=5)
+    # enc = resf(state)
+    # cond_mlp = CondMLP(64*24*24, 2, 16)
+    # pred_noise = cond_mlp(sample=action, timestep=t,
+    #                       encoder_hidden_states=enc)
+
+    # print(pred_noise.shape)
+
+
+    ############# Image net 2
+
+    state = torch.randn(32, 5, 96,96)
+    action = torch.randn(32, 1, 96,96)
+    t = torch.randint(0,100, (32,))
+
+    resf = resnet.resnet_N(layers=[2,2,2], features_only=True, num_input_channels=5)
+    enc = resf(state)
+    cond_mlp = LightCondUnet2D(256,1,16)
+    pred_noise = cond_mlp(sample=action, timestep=t,
+                          encoder_hidden_states=enc)
+
+    print(pred_noise.shape)
+
 
     pass

train_diffusion.py
@@ -168,8 +168,7 @@ def build_diff_trainer(cfg, robot_type, num_diffusion_iter=100,
     state_channel = cfg.num_input_channels
     action_channel = VectorEnv.get_num_output_channels(robot_type)
     # net
-    conditional_encoder = resnet.resnet_N(layers=[2,], features_only=True, num_input_channels=5)
-    noise_model = networks.CondMLP(64*24*24, 2, 16).to(device)
+    # qmap learning network 1
     # conditional_encoder = networks.LightWeightBottleneck(state_channel).to(device)
     # conditional_encoder = networks.LightEncoderFCN(state_channel).to(device)
     # noise_model = networks.LightConditionalNetwork(
@@ -177,6 +176,13 @@ def build_diff_trainer(cfg, robot_type, num_diffusion_iter=100,
     #     256,
     #     repeat_enc=False
     #     ).to(device)
+    # qmap learning network 2
+    conditional_encoder = resnet.resnet_N(layers=[2,2,2], features_only=True,
+                                          num_input_channels=state_channel).to(device)
+    noise_model = networks.LightCondUnet2D(256,action_channel,16)
+    # action space learning network 1
+    # conditional_encoder = resnet.resnet_N(layers=[2,], features_only=True, num_input_channels=5)
+    # noise_model = networks.CondMLP(64*24*24, 2, 16).to(device)
     noise_scheduler = DDPMScheduler(
         num_train_timesteps=num_diffusion_iter,
         beta_schedule='squaredcos_cap_v2',
@@ -184,6 +190,7 @@ def build_diff_trainer(cfg, robot_type, num_diffusion_iter=100,
     ema = EMAModel(
         parameters=noise_model.parameters(),
         power=0.75)
+    ema.to(device)
     policy = DiffusionPolicy(
         inp_dims=(state_channel, height, width),
         output_dims=(action_channel, height, width),
@@ -196,32 +203,34 @@ def build_diff_trainer(cfg, robot_type, num_diffusion_iter=100,
     # trainer
     training_start = np.round(cfg.learning_starts_frac * cfg.total_timesteps).astype(np.uint32)
     num_training_steps = np.ceil((cfg.total_timesteps - training_start)/ cfg.train_freq).astype(np.uint32)
-    # trainer = TDErrorQMapDiffTrainer(
-    #     diff_model=policy,
-    #     discount_factor=0.9,
-    #     num_training_steps=num_training_steps,
-    #     target_transfer_period=cfg.target_update_freq,
-    #     boot_strapped_policy=bootstrap_policy)
-    policy = StateBasedDiffuser(
-        inp_dims=(state_channel,height,width), output_dims=(2,),
-        num_diffusion_iter=num_diffusion_iter,
-        noise_model=noise_model,
-        conditional_encoder=conditional_encoder,
-        noise_scheduler=noise_scheduler,
-        ema=ema,
-        tensor_kwargs=tensor_kwargs).to(device)
-    twin_q = TwinQNetwork(
-        networks.FCN(state_channel, action_channel),
-        networks.FCN(state_channel, action_channel)).to(device)
-    trainer = ActionDiffuserTrainer(
+    trainer = TDErrorQMapDiffTrainer(
         diff_model=policy,
-        critic_model=twin_q,
-        num_training_steps=num_training_steps,
         discount_factor=0.9,
-        policy_eta=1.0,
-        update_ema_every=5,
-        critic_tau=0.005,
-        critic_xfer_period=2)
+        num_training_steps=num_training_steps,
+        target_transfer_period=cfg.target_update_freq,
+        boot_strapped_policy=bootstrap_policy,
+        init_tau=1.0, tau_decay=1.0)
+    # state based learning
+    # policy = StateBasedDiffuser(
+    #     inp_dims=(state_channel,height,width), output_dims=(2,),
+    #     num_diffusion_iter=num_diffusion_iter,
+    #     noise_model=noise_model,
+    #     conditional_encoder=conditional_encoder,
+    #     noise_scheduler=noise_scheduler,
+    #     ema=ema,
+    #     tensor_kwargs=tensor_kwargs).to(device)
+    # twin_q = TwinQNetwork(
+    #     networks.FCN(state_channel, action_channel),
+    #     networks.FCN(state_channel, action_channel)).to(device)
+    # trainer = ActionDiffuserTrainer(
+    #     diff_model=policy,
+    #     critic_model=twin_q,
+    #     num_training_steps=num_training_steps,
+    #     discount_factor=0.9,
+    #     policy_eta=1.0,
+    #     update_ema_every=5,
+    #     critic_tau=0.005,
+    #     critic_xfer_period=2)
 
     return policy, trainer
 
@@ -360,7 +369,7 @@ def encode_intentions(states: Iterable[Iterable],
 
 ########
 
-def main(cfg, log_scalars=True, log_visuals=False):
+def main(cfg, log_scalars=True, log_visuals=True):
     # Set up logging and checkpointing
     log_dir = Path(cfg.log_dir)
     checkpoint_dir = Path(cfg.checkpoint_dir)
@@ -529,9 +538,10 @@ def main(cfg, log_scalars=True, log_visuals=False):
                     random_states, enc_info = encode_intentions(random_states, int_encoders, debug=True)
                 _, act_info = query_actions(random_states, policies, bootstrapped_policies, exploration_eps=0.0, debug=True)
                 for i in range(num_robot_groups):
-                    visualization = utils.get_state_output_visualization(
-                        random_states[i][0], act_info['output'][i][0]).transpose((2, 0, 1))
-                    visualization_summary_writer.add_image('output/robot_group_{:02}'.format(i + 1), visualization, timestep + 1)
+                    if 'output' in act_info:
+                        visualization = utils.get_state_output_visualization(
+                            random_states[i][0], act_info['output'][i][0]).transpose((2, 0, 1))
+                        visualization_summary_writer.add_image('output/robot_group_{:02}'.format(i + 1), visualization, timestep + 1)
                     if cfg.use_predicted_intention:
                         visualization_intention = utils.get_state_output_visualization(
                             random_states[i][0],